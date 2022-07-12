fed8d4aac1d3471eeb20238c2ee617ade68b79bd
roqueCoronel <rokecoronel748@gmail.com>
2022-07-12 12:28:26 -0300

    creamos barajas de cartas, usamos undercore para dar una baraja de cartas removida

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,1 +1,31 @@
-console.log('hello')
+// 2c es un 2 de treboles 
+// 2d de diamonds
+// 2h two of hearts 
+// 2s twp of spades 
+
+let deck = []; 
+const tipos = ['C','D','H','S'];
+const especiales = ['A','J','Q','K'];
+const crearDeck = ()=>{
+    for( let i = 2; i <= 10 ; i++){
+       for( let tipo of tipos) {
+        deck.push(i + tipo)
+       } 
+       
+       for(let tipo of tipos) {
+          for(let esp of especiales){
+            deck.push(esp +  tipo)
+          }
+       }
+    
+    }
+
+  
+    console.log(deck);
+
+    deck = _.shuffle(deck);
+    console.log(deck)
+    return deck;
+}
+
+crearDeck();
